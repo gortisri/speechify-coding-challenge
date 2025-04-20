11fb94d0eeaad1bec6a7db187220c5836301fa7d
sri g <sri@sris-MacBook-Pro.local>
2025-04-20 11:40:29 -0500

    Initial test commit

---
+++ SpeechifyCodingChallenge/SpeechifyCodingChallenge/SpeechifyCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechifyCodingChallengeApp.swift
+//  SpeechifyCodingChallenge
+//
+//  Created by sri g on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechifyCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeechifyCodingChallenge/SpeechifyCodingChallengeTests/SpeechifyCodingChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpeechifyCodingChallengeTests.swift
+//  SpeechifyCodingChallengeTests
+//
+//  Created by sri g on 4/20/25.
+//
+
+import Testing
+
+struct SpeechifyCodingChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
